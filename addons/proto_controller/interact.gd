493b452d24352ddada9402a1691c24368eecb0a3
GlebEvemi <gleb2466@gmail.com>
2026-06-22 23:23:09 +0300

    add raycast

---
+++ addons/proto_controller/interact.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var raycast:RayCast3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("interact"):
+		if raycast.is_colliding():
+			var obj = raycast.get_collider()
+			print(obj.name)
+			if obj.has_method("on_interact"):
+				obj.on_interact()
---
+++ object_interaction.gd
@@ -0,0 +1,9 @@
+extends Node3D
+class_name Box
+
+
+func _ready():
+	pass
+
+func on_interact() -> void:
+	print("Singal emitted")
